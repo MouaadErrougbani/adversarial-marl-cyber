f136dd06c8b43e1c21d5fb286879d9a1cd66b821
MouaadErrougbani <mouaad.errougbani@usmba.ac.ma>
2026-06-03 18:58:00 +0000

    M

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ HYPER_PARAMS = SimpleNamespace(
     workers = 2,       # How many envs can run in parallel
     bs = 2500,          # How many steps to learn from at a time
     episode_len = 500,
-    training_episodes = 50_000, # Realistically, stops improving around 50k
+    training_episodes = 25, # Realistically, stops improving around 50k
     epochs = 4
 )
 
